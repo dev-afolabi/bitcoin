4490b908a53a1f5fe36661e4dd3453414f63fbe7
Dauda Ahmed Afolabi <dauda.ahmeda@gmail.com>
2020-07-22 18:32:24 -0800

    fixed error page

--- bitcoin_pages/urls.py
+++ bitcoin_pages/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about-us$', views.about, name='about-us'),
+    url(r'^contact-us$', views.contact, name='contact'),
+    url(r'^frequently-asked-question$', views.faq, name='faq'),
 ]
--- bitcoin_pages/views.py
+++ bitcoin_pages/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):        
     return render(request, 'bitcoin_pages/index.html')
+
+def about(request):        
+    return render(request, 'bitcoin_pages/about-us.html')
+
+def contact(request):        
+    return render(request, 'bitcoin_pages/contact-us.html')
+
+def faq(request):        
+    return render(request, 'bitcoin_pages/faq.html')
