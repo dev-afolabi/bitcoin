c5893fa5c214e1f7ba08b1b5ae31f3c0506fe8ba
Dauda Ahmed Afolabi <dauda.ahmeda@gmail.com>
2020-08-14 22:40:54 -0800

    removed pre-loader

--- users/views.py
+++ users/views.py
@@ -40,7 +40,7 @@ def signup(request):
                 'user':user,
                 'site_name':current_site,
                 'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
                 'token':account_activation_token.make_token(user)
             })
             to_email = bound_form.cleaned_data.get('email')
