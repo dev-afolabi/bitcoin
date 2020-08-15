04e1ded75e63b6ef47d33966a781dea07437a920
Dauda Ahmed Afolabi <dauda.ahmeda@gmail.com>
2020-08-14 23:25:32 -0800

    removed pre-loader

--- users/token.py
+++ users/token.py
@@ -4,6 +4,6 @@ import six
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            six.text_type(user.pk)+six.text_type(timestamp)+six.text_type(user.username)
+            six.text_type(user.pk)+six.text_type(timestamp)+six.text_type(user.email)
         )
 account_activation_token = AccountActivationTokenGenerator()
