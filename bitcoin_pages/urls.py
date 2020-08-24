a713ca8cc53b9cfed01e1c25dc32d5fcf3cbdc9a
Dauda Ahmed Afolabi <dauda.ahmeda@gmail.com>
2020-08-24 02:16:44 -0800

    working on the ui

--- bitcoin_pages/urls.py
+++ bitcoin_pages/urls.py
@@ -1,11 +1,20 @@
 from django.conf.urls import url
 
-from . import views
+from .views import *
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^about-us$', views.about, name='about-us'),
-    url(r'^contact-us$', views.contact, name='contact'),
-    url(r'^frequently-asked-question$', views.faq, name='faq'),
-    url(r'^services$', views.services, name='services'),
+    url(r'^$', index, name='index'),
+    url(r'^about-us$', about, name='about-us'),
+    url(r'^contact-us$', contact, name='contact'),
+    url(r'^frequently-asked-question$', faq, name='faq'),
+    url(r'^services$', services, name='services'),
+    url(r'^bitcoin-and-ethereum$', bitcoin_ethereum, name='bitcoin_ethereum'),
+    url(r'^bitcoin-legality$', bitcoin_legality, name='bitcoin_legality'),
+    url(r'^bitcoin-trading$', bitcoin_trading, name='bitcoin_trading'),
+    url(r'^coin-buy-and-sell$', coin_buy, name='coin_buy'),
+    url(r'^crypto-trading$', crypto_trading, name='crypto_trading'),
+    url(r'^company-profile$', profile, name='profile'),
+    url(r'^security-ensure$', security_ensure, name='security_ensure'),
+    url(r'^refund-policy$', refund_policy, name='refund_policy'),
+    url(r'^terms-and-condition$', terms_and_condition, name='terms_and_condition'),
 ]
--- bitcoin_pages/views.py
+++ bitcoin_pages/views.py
@@ -15,3 +15,30 @@ def faq(request):
 
 def services(request):        
     return render(request, 'bitcoin_pages/services.html')
+
+def bitcoin_ethereum(request):        
+    return render(request, 'bitcoin_pages/bitcoin_and_ethereum.html')
+
+def bitcoin_legality(request):        
+    return render(request, 'bitcoin_pages/bitcoin_legality.html')
+
+def coin_buy(request):        
+    return render(request, 'bitcoin_pages/coin_buy_and_sell.html')
+
+def crypto_trading(request):        
+    return render(request, 'bitcoin_pages/crypto_trading.html')
+
+def bitcoin_trading(request):        
+    return render(request, 'bitcoin_pages/bitcoin_trading.html')
+
+def security_ensure(request):        
+    return render(request, 'bitcoin_pages/security_ensure.html')
+
+def profile(request):        
+    return render(request, 'bitcoin_pages/profile.html')
+
+def refund_policy(request):        
+    return render(request, 'bitcoin_pages/refund_policy.html')
+
+def terms_and_condition(request):        
+    return render(request, 'bitcoin_pages/terms_and_condition.html')
