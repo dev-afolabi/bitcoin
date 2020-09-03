1733548cd1d6861025c4b8bd6c479e77e401e5ca
Dauda Ahmed Afolabi <dauda.ahmeda@gmail.com>
2020-09-03 15:31:17 +0100

    change admin name

--- users/admin.py
+++ users/admin.py
@@ -10,4 +10,8 @@ class UserAdmin(admin.ModelAdmin):
   
     active.boolean = True
 
+admin.site.site_header = 'Fonixcoin Trade Center Administration'
+admin.site.site_title = 'Fonixcoin Trade Center'
+admin.site.index_title = 'Fonixcoin Trade Center Administration'
+
 admin.site.register(User, UserAdmin)
