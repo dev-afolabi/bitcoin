284a4726c92dcf63fa7aa3099686a8cc00645ce2
Dauda Ahmed Afolabi <dauda.ahmeda@gmail.com>
2020-09-03 15:36:06 +0100

    added admin.py to site

---
+++ bitcoin/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+admin.site.site_header = 'Fonixcoin Trade Center Administration'
+admin.site.site_title = 'Fonixcoin Trade Center'
+admin.site.index_title = 'Fonixcoin Trade Center Administration'
