939cacadf2ad8ca787a1d8430c85b686183e2dbb
Dauda Ahmed Afolabi <dauda.ahmeda@gmail.com>
2020-09-10 15:21:26 +0100

    started translation

--- bitcoin_pages/forms.py
+++ bitcoin_pages/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-
+from django.utils.translation import ugettext_lazy as _
 
 class ContactForm(forms.Form):
-    fullname = forms.CharField(label="Fullname",widget=forms.TextInput(attrs={'placeholder':'Your Fullname'}),max_length=100)
-    from_email = forms.CharField(label="Email",widget=forms.EmailInput(attrs={'placeholder':'Your Email'}))
-    subject = forms.CharField(label="Subject",widget=forms.TextInput(attrs={'placeholder':'Subject'}),max_length=300)
-    message = forms.CharField(label="message",widget=forms.Textarea(attrs={'placeholder':'Your Message'}))
+    fullname = forms.CharField(label="Fullname",widget=forms.TextInput(attrs={'placeholder':_('Your Fullname')}),max_length=100)
+    from_email = forms.CharField(label="Email",widget=forms.EmailInput(attrs={'placeholder':_('Your Email')}))
+    subject = forms.CharField(label="Subject",widget=forms.TextInput(attrs={'placeholder':_('Subject')}),max_length=300)
+    message = forms.CharField(label="message",widget=forms.Textarea(attrs={'placeholder':_('Your Message')}))
